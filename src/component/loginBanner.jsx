619e77fc07ec914be3fbb92ffc7fcba10a06dd24
varshan2026 <avarshan2026@gmail.com>
2025-03-29 21:50:17 +0530

    tailwind CSS

--- src/component/loginBanner.jsx
+++ src/component/loginBanner.jsx
@@ -36,13 +36,13 @@ function LoginBanner() {
 
   return (
     <div
-      className="loginbanner-container w-[100vw] xl:w-[50vw] h-[75vh] md:h-[90vh] xl:h-[100vh] flex justify-center items-center bg-[#3c6e71] bg-no-repeat bg-[length:auto] bg-[-320px_255px]"
+      className="loginbanner-container w-[100vw] xl:w-[50vw] h-[70vh] md:h-[90vh] xl:h-[100vh] flex justify-center items-center bg-[#3c6e71] bg-no-repeat bg-[length:auto] bg-[-320px_255px]"
       style={{ backgroundImage: `url(${round})` }}
     >
       <div className="flex justify-center items-center">
         <motion.div
           key={currentIndex}
-          className="h-[65vh] xl:h-[90vh] w-[86vw] xl:w-[45vw] border rounded-lg border-white px-6.5 py-6 bg-white/12 backdrop-blur-sm relative"
+          className="h-[60vh] xl:h-[90vh] w-[86vw] xl:w-[45vw] border rounded-lg border-white px-6 py-6 bg-white/12 backdrop-blur-sm relative"
         >
           <motion.h1
             className="text-xl xl:text-4xl font-semibold text-white lg:mt-4"
@@ -54,15 +54,15 @@ function LoginBanner() {
             {slideDetail[currentIndex].title}
           </motion.h1>
 
-          <motion.h5
-            className="text-lg xl:text-xl text-white mt-4 xl:mt-5 leading-7 w-70 xl:w-90"
+          <motion.h6
+            className="text-md xl:text-xl text-white mt-4 xl:mt-5 leading-6 xl:leading-7 w-70 xl:w-90"
             initial={{ opacity: 0, x: 0 }}
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: -50 }}
             transition={{ duration: 0.9 }}
           >
             {slideDetail[currentIndex].para}
-          </motion.h5>
+          </motion.h6>
 
           <motion.img
             src={slideDetail[currentIndex].img}
