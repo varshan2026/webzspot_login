d34ef11ee634d8e7a709c9690108658a18e17b9e
varshan2026 <avarshan2026@gmail.com>
2025-03-30 20:27:39 +0530

    update css

--- src/component/LoginForm.jsx
+++ src/component/LoginForm.jsx
@@ -52,7 +52,7 @@ function LoginForm() {
   };
 
   return (
-    <div className="!px-6 my-10 flex justify-center items-center w-[100vw] xl:w-[50vw]">
+    <div className="px-8 my-12 flex justify-center items-center w-[100vw] xl:w-[50vw]">
       <div className="flex flex-col gap-y-10">
         <div>
           <h1 className="text-4xl text-gray-800 font-semibold">
@@ -139,6 +139,7 @@ function LoginForm() {
         </div>
       </div>
     </div>
+    
   );
 }
 
